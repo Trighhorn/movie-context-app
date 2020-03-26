c56fe55694c6c33a296b5aff0176a712b35bc8c2
Abrahan <trighhorn@gmail.com>
2020-03-26 13:34:34 -0600

    started building app with context

---
+++ src/contexts/MovieContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+export const MovieContext = createContext()
+
+const MovieContextProvider = (props) => {
+    const [movies, setMovies] = useState([
+        {title: 'Movie One', genre: 'Comedy', id: 1},
+        {title: 'Movie Two', genre: 'Romance', id: 2}
+    ])
+
+    const addMovie = (title, genre) => {
+        setMovies([...movies, {title: title, genre: genre, id: uuidv4()}])
+    }
+
+    const removeMovie = (id) => {
+        setMovies(movies.filter(movie => movie.id !== id))
+    }
+
+    return (
+        <MovieContext.Provider value={{movies, addMovie: addMovie, removeMovie: removeMovie}}>
+            {props.children}
+        </MovieContext.Provider>
+    );
+}
+ 
+export default MovieContextProvider;
